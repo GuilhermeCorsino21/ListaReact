9d81c0aafc0a9c3fbecfb3815cb39326b9962e37
Guilherme <guilhermecorsino21@gmail.com>
2023-02-05 19:19:25 -0300

    alterando nome e tempo

--- reactapp/src/pages/Home/index.jsx
+++ reactapp/src/pages/Home/index.jsx
@@ -6,22 +6,40 @@ import { Card } from '../../components/Card';
 function Home() {
   //O estado tem 2 elementos, o elemento em si(studentName) e a função q muda o estado(set...)//s
   const [studentName, setStudentName] = useState();
+  const [students, setStudents] = useState([]);
+
+  function AddStudent(){
+    const newStudent = {
+      name: studentName, 
+      time: new Date().toLocaleTimeString("pt-br", {
+        hours:'2-digit', 
+        minute: '2-digit',
+        seconds: '2-digit'
+      })
+    };
+
+    setStudents([newStudent]);
+
+  }
 
   return (
     <div className='container'>
-      <h1>Nome: {studentName} </h1>
+      <h1>Lista de Presença: </h1>
       <input type="text" 
       placeholder="Digite o nome.." 
       // função para atualizar o estado //
       onChange = {e => setStudentName(e.target.value)}
       />
       
-      <button type="button">Adicionar</button>
+      <button type="button" onClick={AddStudent}>
+        Adicionar
+      </button>
       
-      <Card name='Luiz' time='18:32' />
-      <Card name='Guilherme' time='18:40' />
-      <Card name='Aleatório' time='20:50' />
-      
+      {
+        students.map(student => <Card name={student.name} time={student.time} />)
+        
+        
+      }
       
     
     </div>
